d2d54339e22d6b30e87cd5e701bf3538a65463c9
Kalysh <503292@ukr.net>
2019-10-04 16:37:18 +0300

    Updates

---
+++ precache-manifest.dcb7c1d26a7959499049aaeb7789c36d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72981cb7d9354b0e106071502b6d033",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "942dd01b92d5d6ec252f",
+    "url": "/goit-react-hw-02-reader/static/css/main.d5547ec9.chunk.css"
+  },
+  {
+    "revision": "4728b2de135e519fe3f2",
+    "url": "/goit-react-hw-02-reader/static/js/2.ac132628.chunk.js"
+  },
+  {
+    "revision": "942dd01b92d5d6ec252f",
+    "url": "/goit-react-hw-02-reader/static/js/main.1d658ff4.chunk.js"
+  },
+  {
+    "revision": "016cf866b71c8aab7d58",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.47041a14.js"
+  }
+]);
